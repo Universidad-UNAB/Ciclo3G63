fb7b82c7624e50758739da440013aece4e48c3d4
Jesús González <jesus.gonzalez@corhuila.edu.co>
2022-09-28 22:08:13 -0500

    Semana 4 sesion 1

---
+++ Semana4/Sesión1/proyecto/js/Tarea1.js
@@ -0,0 +1,32 @@
+function Calcular(x){
+    /**Alternativa 1*/
+    if(x==1){
+
+    }
+    if(x==2){
+        
+    }
+    if(x==3){
+        
+    }
+
+    /**Alternativa 2*/
+    if(x==1){
+        
+    }else if(x==2){
+        
+    }else if(x==3){
+        
+    }
+
+    /**Alternativa 3 */
+    switch(x){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3: 
+            break;
+    }
+
+}
